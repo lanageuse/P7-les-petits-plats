cf73ecff6799ec22e625c59b53fe6b896be7e410
Dev Lanageuse <dev@lanageuse.com>
2025-03-11 09:34:11 +0100

    Refactor(recipe): suppression des getters et simplification de la classe Recipe
    - Suppression des propriétés privées `_` et des getters pour un accès direct aux données.

--- scripts/models/recipe.js
+++ scripts/models/recipe.js
@@ -1,25 +1,15 @@
 class Recipe {
     constructor(data) {
-        this._id = data.id
-        this._image = data.image
-        this._name = data.name
-        this._servings = data.servings
-        this._ingredients = data.ingredients
-        this._time = data.time
-        this._description = data.description
-        this._appliance = data.appliance
-        this._ustensils = data.ustensils
+        this.id = data.id
+        this.image = data.image
+        this.name = data.name
+        this.servings = data.servings
+        this.ingredients = data.ingredients
+        this.time = data.time
+        this.description = data.description
+        this.appliance = data.appliance
+        this.ustensils = data.ustensils
     }
-
-    get id() { return this._id }
-    get image() { return this._image }
-    get name() { return this._name }
-    get servings() { return this._servings }
-    get ingredients() { return this._ingredients }
-    get time() { return this._time }
-    get description() { return this._description }
-    get appliance() { return this._appliance }
-    get ustensils() { return this._ustensils }
 }
 
 export default Recipe
