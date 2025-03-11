5e6ca2d453882ca7effbf0676a79f909ca1fb85d
Dev Lanageuse <dev@lanageuse.com>
2025-03-11 09:35:07 +0100

    Refactor(index): amélioration de la gestion des recettes et de l'affichage des résultats
    - Ajout de `UpdateResult` pour gérer la mise à jour des résultats de recherche.
    - Séparation de `recipes` et `filteredRecipes` pour mieux gérer les filtres.
    - Réinitialisation de l'affichage des cartes recettes avant chaque mise à jour.
    - Intégration dynamique des dropdowns avec mise à jour des filtres.
    - Suppression de `_recipes` au profit de `recipes` et `filteredRecipes` pour plus de clarté.

--- app.js
+++ app.js
@@ -2,36 +2,49 @@ import Api from './scripts/api/Api.js'
 import Recipe from './scripts/models/recipe.js'
 import RecipeCard from './scripts/components/recipeCard.js'
 import DropDown from './scripts/components/dropdown/Dropdown.js'
+import UpdateResult from './scripts/utils/UpdateResult.js'
 class Index {
     constructor() {
         this.recipesApi = new Api('./data/recipes.json')
-        this._recipes = []
         this.$wrapper = document.getElementById("recipes_wrapper")
-        this.DropDown = DropDown
+        this.recipes = []
+        this.filteredRecipes = []
+        this.dropdowns = []
     }
 
     async getRecipes() {
         const recipesData = await this.recipesApi.get()
-        this._recipes = recipesData.map(data => new Recipe(data))
+        this.recipes = recipesData.map(data => new Recipe(data))
+        this.filteredRecipes = [...this.recipes]
     }
 
-    async displayCardsRecipes() {
-        this._recipes.forEach((recipe) => {
+     async displayCardsRecipes(recipes) {
+        this.$wrapper.innerHTML = ''
+        recipes.forEach((recipe) => {
             const card = new RecipeCard(recipe)
             const template = card.createCard();
             this.$wrapper.insertAdjacentHTML("beforeend", template);
         })
     }
-
     async main() {
         await this.getRecipes()
-        await this.displayCardsRecipes()
-        document.querySelectorAll('.dropdown').forEach(dropdown => {
-            new DropDown(dropdown);
-        })
-    }
+        await this.displayCardsRecipes(this.filteredRecipes)
 
+        document.querySelectorAll('.dropdown').forEach(dropdown => {
+            const dropdownInstance = new DropDown(dropdown, this)
+            this.dropdowns.push(dropdownInstance)
+        })
+
+        this.dropdowns.forEach(dropdown => 
+            dropdown.dropDownList.fetchDropdownItems(this.filteredRecipes)
+        )
+
+        new UpdateResult(this)
+
+    }
 }
 
+export default Index
+
 const app = new Index()
-app.main()
+await app.main()
